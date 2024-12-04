dab78022a7c5596604cd5d5bda4e58a7186120ac
Marsinex <hectatuctobblu@gmail.com>
2024-12-04 23:06:15 +0500

    code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.IO;
+using System;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Principal;
+using LinqToExcel.Extensions;
+using System.Security.Cryptography;
+using System.ComponentModel;
+
+namespace WPFapp1
+{
+    public partial class MainWindow : Window
+    {
+        basa b;
+        public MainWindow()
+        {
+            MessageBoxResult r= MessageBox.Show("Писать логи в новый файл?","Логи", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            b = new basa(r==MessageBoxResult.Yes);
+            InitializeComponent();
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            display.Text = "";
+            switch (choise.SelectedIndex)
+            {
+                case 0:
+                    display.Text=b.printDict(b.cur);
+                    l1.Content = "ID";
+                    l2.Content = "Обозначение";
+                    l3.Content = "Курс";
+                    l4.Content = "Название";
+                    l5.Content = "";
+                    break;
+                case 1:
+                    display.Text = b.printDict(b.inc);
+                    l1.Content = "ID";
+                    l2.Content = "ID счёта";
+                    l3.Content = "ID валюты";
+                    l4.Content = "Дата";
+                    l5.Content = "Сумма";
+                    break;
+                case 2:
+                    display.Text = b.printDict(b.acc);
+                    l1.Content = "ID";
+                    l2.Content = "Имя";
+                    l3.Content = "Дата";
+                    l4.Content = "";
+                    l5.Content = "";
+                    break;
+            }
+        }
+        private void wannaDo(object sender, SelectionChangedEventArgs e)
+        {
+            switch (delat.SelectedIndex)
+            {
+                case 0://добавить
+                   
+                    l1.Visibility = Visibility.Hidden;
+                    l2.Visibility = Visibility.Visible;
+                    l3.Visibility = Visibility.Visible;
+                    l4.Visibility = Visibility.Visible;
+                    l5.Visibility = Visibility.Visible;
+                    break;
+                case 1://удалить
+                   
+                    l1.Visibility = Visibility.Visible;
+                    l2.Visibility = Visibility.Hidden;
+                    l3.Visibility = Visibility.Hidden;
+                    l4.Visibility = Visibility.Hidden;
+                    l5.Visibility = Visibility.Hidden;
+                    break;
+                case 2://редактировать
+                   
+                    l1.Visibility = Visibility.Visible;
+                    l2.Visibility = Visibility.Visible;
+                    l3.Visibility = Visibility.Visible;
+                    l4.Visibility = Visibility.Visible;
+                    l5.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                b.Dosmth(choise.SelectedIndex, delat.SelectedIndex, int.Parse(t1.Text), t2.Text, t3.Text, t4.Text, t5.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Чтото ты напутал с вводом, проверь на всякий", "Ошиблися", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            ComboBox_SelectionChanged(null, null);//обновляю выведенную таблицу
+
+        }
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            b.save();
+        }
+        private void t1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!int.TryParse(t1.Text,out int i)) t1.Text = "0";
+        }
+    }
+}
---
+++ basa.cs
@@ -0,0 +1,173 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace WPFapp1
+{
+    struct accaunt
+    {
+        string name;
+        string date;
+        public accaunt(string n, string t)
+        {
+            name = n;
+            date = t;
+        }
+        override public string ToString()
+        {
+            return name + " " + date;
+        }
+
+    }
+    struct curs
+    {
+        string letter;
+        float ko;
+        string name;
+        public curs(string l, float k, string n)
+        {
+            letter = l;
+            ko = k;
+            name = n;
+        }
+        override public string ToString()
+        {
+            return letter + " " + ko + " " + name;
+        }
+    }
+    struct income
+    {
+        int aid;
+        int cid;
+        string date;
+        float sum;
+        public income(int aid, int cid, string date, float sum)
+        {
+            this.aid = aid;
+            this.cid = cid;
+            this.date = date;
+            this.sum = sum;
+        }
+        override public string ToString()
+        {
+            return aid + " " + cid + " " + date + " " + sum;
+        }
+    }
+    internal class basa
+    {
+        public Dictionary<int, accaunt> acc;
+        public Dictionary<int, curs> cur;
+        public Dictionary<int, income> inc;
+        StreamWriter log;
+        public basa(bool logNew)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+            if (logNew) log = new StreamWriter("log" + DateTime.Now.ToString().Replace(':','-').Replace('.','-') + ".txt");
+            else log = new StreamWriter("log.txt",true);
+            LinqToExcel.ExcelQueryFactory excelFile=null;
+            try { 
+                excelFile = new LinqToExcel.ExcelQueryFactory(@"LR6-var13.xls");
+                toLog("Открыта БД");
+            }
+            catch{
+                toLog("Не найдена база данных");
+                return;
+            }
+            var a = from shet in excelFile.Worksheet(0) select shet;
+            cur = a.ToDictionary(row => int.Parse(row[0].ToString()), row => new curs(row[1], float.Parse(row[2]), row[3]));
+            a = from shet in excelFile.Worksheet(1) select shet;
+            inc = a.ToDictionary(row => int.Parse(row[0].ToString()), row => new income(int.Parse(row[1]), int.Parse(row[2]), row[3], float.Parse(row[4])));
+            a = from shet in excelFile.Worksheet(2) select shet;
+            acc = a.ToDictionary(row => int.Parse(row[0].ToString()), row => new accaunt(row[1], row[2]));
+        }
+        private void toLog(string l)
+        {
+            log.Write(DateTime.Now+" "+l+"\n");
+        }
+        public void save()
+        {
+            toLog("Закрытие программы");
+            log.Close();
+        }
+        public string printDict<T>(Dictionary<int, T> d) where T : struct
+        {
+            string o = "";
+            if (d.Count < 50)
+                foreach (var i in d) o += i.Key + " " + i.Value + "\n";
+            else
+            {
+                int c = 0;
+                foreach (var i in d)
+                {
+                    if (c++ < 50) o += i.Key + " " + i.Value + "\n";
+                    else return o;
+                }
+            }
+            return o;
+        }
+        public void Dosmth(int c,int d,int t1,string t2,string t3,string t4,string t5)
+        {
+            switch (d)
+            {
+                case 0:
+                    switch (c)
+                    {
+                        case 0:
+                            cur[cur.Count] = new curs(t2, float.Parse(t3), t4);
+                            toLog("В таблицу 'Курсы валют' добавлена запись:"+cur[cur.Count-1].ToString());
+                            break;
+                        case 1:
+                            inc[inc.Count] = new income(int.Parse(t2),int.Parse(t3),t4,float.Parse(t5));
+                            toLog("В таблицу 'Поступления' добавлена запись:" + inc[inc.Count-1].ToString());
+                            break;
+                        case 2:
+                            acc[acc.Count] = new accaunt(t2, t3);
+                            toLog("В таблицу 'Счета' добавлена запись:" + acc[acc.Count-1].ToString());
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (c)
+                    {
+                        case 0:
+                            toLog("Из таблицы 'Курсы валют' удалена запись:" + cur[t1].ToString());
+                            cur.Remove(t1);
+                            break;
+                        case 1:
+                            toLog("Из таблицы 'Поступления' удалена запись:" + inc[t1].ToString());
+                            inc.Remove(t1);
+                            break;
+                        case 2:
+                            toLog("Из таблицы 'Счета' удалена запись:" + acc[t1].ToString());
+                            acc.Remove(t1);
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (c)
+                    {
+                        case 0:
+                            toLog("В таблице 'Курсы валют' изменена запись:" + cur[t1].ToString());
+                            cur[t1] = new curs(t2, float.Parse(t3), t4);
+                            break;
+                        case 1:
+                            toLog("В таблице 'Поступления' изменена запись:" + inc[t1].ToString());
+                            inc[t1] = new income(int.Parse(t2), int.Parse(t3), t4, float.Parse(t5));
+                            break;
+                        case 2:
+                            toLog("В таблице 'Счета' изменена запись:" + acc[t1].ToString());
+                            acc[t1] = new accaunt(t2, t3);
+                            break;
+                    }
+                    break;
+            }
+
+        }
+    }
+}
